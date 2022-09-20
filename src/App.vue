f3ea95343b04e0ea40f41358ed35b461a018e90e
Erol YILDIZ <erol_yildiz@msn.com>
2022-09-20 16:14:31 +0300

    second

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
-<script setup>
-import Filtre from './components/popover.vue';
-import Popover from './components/popover.vue';
-</script>
-
 <template>
- 
-    <Popover />
+   <div class="container d-block border py-3 my-3">
+        
+        <Popover />
+    </div>
 
+ 
 </template>
 
+<script>
+import Popover from "./components/popover.vue";
+export default {
+  name: "app",
+  components: {
+    Popover
+  }
+};
+</script>
+
+<style>
+#app {
+
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  top: 0px;
+  position:absolute;
+  width: 100%;
+  left: 0;
+  right: 0;
+}
+</style>
--- src/components/filtre.vue
+++ src/components/filtre.vue
@@ -1,46 +1,46 @@
 <template>
     <div>
-      <input type="radio" class="btn-check" name="options-outlined" id="all" autocomplete="off" checked>
+      <input type="radio" class="btn-check" name="options-outlined"   v-model="selectedRadio" value="" id="all" autocomplete="off" checked>
       <label class="btn btn-outline-info rounded-pill mx-2" for="all">Hepsi</label>
 
-      <input type="radio" class="btn-check" name="options-outlined" id="sifir" autocomplete="off">
+      <input type="radio" class="btn-check" name="options-outlined"    v-model="selectedRadio" value="0" id="sifir" autocomplete="off">
       <label class="btn btn-outline-info rounded-pill mx-2" for="sifir">BingX Vadeli İşlemler</label>
 
-      <input type="radio" class="btn-check" name="options-outlined" id="iki" autocomplete="off">
+      <input type="radio" class="btn-check" name="options-outlined"   v-model="selectedRadio" value="2" id="iki" autocomplete="off">
       <label class="btn btn-outline-info rounded-pill mx-2" for="iki">BingX Sürekli Vadeli</label>
 
-      <input type="radio" class="btn-check" name="options-outlined" id="bir" autocomplete="off">
+      <input type="radio" class="btn-check" name="options-outlined"   v-model="selectedRadio" value="1" id="bir" autocomplete="off">
       <label class="btn btn-outline-info rounded-pill mx-2" for="bir">Binance Vadeli İşlemler</label>
   
       <div class="mb-3">
         <label ><span class="float-start" >Kümülatif PnL (USDT)</span ><span class="float-end"> {{kumulatif_pnl}}-2000+</span></label>
-        <Slider  class="slider-blue"  v-model="kumulatif_pnl" :step="200" :min="0" :max="2000"  :tooltips="false" />
+        <Slider  class="slider-blue"  @change="updateit" v-model="kumulatif_pnl" :step="200" :min="0" :max="2000"  :tooltips="false" />
      </div>
   
      <div class="mb-3">
         <label class="form-label"> <span class="float-start" >Hesap Varlıkları (USDT)</span ><span class="float-end"> {{hesap_varliklari}}-10000+</span></label>
-        <Slider  class="slider-blue"  v-model="hesap_varliklari" :step="1000" :min="0" :max="10000"  :tooltips="false"  />
+        <Slider  class="slider-blue"  @change="updateit" v-model="hesap_varliklari" :step="1000" :min="0" :max="10000"  :tooltips="false"  />
       </div>
 
         <div class="mb-3">
           <label class="form-label"><span class="float-start" >Takipçiler</span ><span class="float-end"> {{takipciler}}-3000+</span></label>
-          <Slider  class="slider-blue"  v-model="takipciler" :step="300" :min="0" :max="3000"  :tooltips="false"/>
+          <Slider  class="slider-blue"  @change="updateit" v-model="takipciler" :step="300" :min="0" :max="3000"  :tooltips="false"/>
        </div>
        <div class="mb-3">
           <label  class="form-label"><span class="float-start" >Kümülatif Kazançlar (USDT)</span ><span class="float-end"> {{kumulatif_kazanc}}-10000+</span></label>
-          <Slider  class="slider-blue"  v-model="kumulatif_kazanc" :step="1000" :min="0" :max="10000"  :tooltips="false"/>
+          <Slider  class="slider-blue"  @change="updateit" v-model="kumulatif_kazanc" :step="1000" :min="0" :max="10000"  :tooltips="false"/>
        </div>
        <div class="mb-3">
           <label  class="form-label"><span class="float-start" >30G ROI</span ><span class="float-end"> {{roi}}-200%+</span></label>
-          <Slider  class="slider-blue"  v-model="roi" :step="20" :min="0" :max="200"  :tooltips="false"/>
+          <Slider  class="slider-blue"  @change="updateit" v-model="roi" :step="20" :min="0" :max="200"  :tooltips="false"/>
        </div>
        <div class="mb-3">
           <label  class="form-label"><span class="float-start" >Risk</span ><span class="float-end"> {{risk}}-9</span></label>
-          <Slider  class="slider-blue"  v-model="risk" :step="1" :min="0" :max="9"  :tooltips="false"/>
+          <Slider  class="slider-blue"  @change="updateit" v-model="risk" :step="1" :min="0" :max="9"  :tooltips="false"/>
        </div>
        <div class="mb-3">
           <label  class="form-label"><span class="float-start" >Başarı Oranı</span ><span class="float-end">  {{basari_oarani}}-100%</span></label>
-          <Slider  class="slider-blue"  v-model="basari_oarani" :step="10" :min="0" :max="100"  :tooltips="false"/>
+          <Slider  class="slider-blue"  @change="updateit" v-model="basari_oarani" :step="10" :min="0" :max="100"  :tooltips="false"/>
        </div>
        
     
@@ -48,7 +48,7 @@
 
 
   <button type="reset" class="btn btn-outline-primary">Clear</button> 
-  <button type="submit" class="btn btn-primary mx-2">Submit</button>
+  <button @click="createParams()" class="btn btn-primary mx-2">Submit</button>
       
     
 </div>
@@ -58,7 +58,7 @@
 
 <script>
   import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+  import "bootstrap"
   import Slider from '@vueform/slider'
 
 export default {
@@ -67,6 +67,7 @@ export default {
   },
   data() {
     return {
+      selectedRadio:null,
       kumulatif_pnl: 0,
       hesap_varliklari:0,
       takipciler:[0,3000],
@@ -74,10 +75,119 @@ export default {
       roi:0,
       risk:[0,9],
       basari_oarani:0,
-      p_l_orani:1
+      p_l_orani:1,
+      get_param:{ conditions:{}, nickName: ""}
+    };
+
+  },
+  methods:{
+    createParams(){
+      console.log(JSON.stringify(this.get_param) );
+    },
+    updateit(v){
+      if(this.kukumulatif_pnl!=0){
+       let obj={       
+        
+            key: "accEarning",
+            type: "range",
+            min: this.kukumulatif_pnl,
+            value: 0
+        }
+        this.get_param.conditions={...this.get_param.conditions,obj}
+        
+      }
+      if(this.hesap_varliklari!=0){
+       let obj={
+        
+            key: "equity",
+            type: "range",
+            min: this.hesap_varliklari,
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.takipciler!=[0,3000]){
+      let obj={
+
+       
+            key: "followerNum",
+            type: "range",
+            min: this.takipciler[0],
+            max: this.takipciler[1],
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.kumulatif_kazanc!=0){ 
+           let obj={
+            key: "followerEarning",
+            type: "range",
+            min: this.kumulatif_kazanc,
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.roi!=0){ 
+          let obj={
+         
+            key: "accEarningRatio",
+            type: "range",
+            min: this.roi,
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.risk!=[0,3000]){
+       let obj={
+
+       
+        
+            key: "riskLevel",
+            type: "range",
+            min: this.risk[0],
+            max: this.risk[0],
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.basari_oarani!=0){ 
+           let obj={
+         
+        
+            key: "winRate",
+            type: "range",
+            min: this.basari_oarani,
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.p_l_orani!=0){ 
+           let obj={
+         
+         
+            key: "pnlRate",
+            type: "range",
+            min: this.p_l_orani,
+            value: 0
+          }
+          this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+          if(this.selectedRadio!=""){ 
+           let obj={
+         
+        
+            key: "exchangeId",
+            type: "singleSelect",
+            selected: this.selectedRadio
+          }
+            this.get_param.conditions={...this.get_param.conditions,obj};
+        }
+       
+       
+     
     }
+  },
   }
-}
   </script>
 <style src="@vueform/slider/themes/default.css"></style>
 <style scoped>
